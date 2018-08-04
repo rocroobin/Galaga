a57438eab802e21660a013bbe03657f8cb9311dd
rocroobin <sizpw@mail.ru>
2018-08-04 12:27:57 +0300

    Запустил движение падающих звезд

---
+++ galaga.js
@@ -0,0 +1,12 @@
+  // звездочки
+  const maxCount = 10;
+  let stars = new Array(maxCount);
+  stars = stars.fill(true);
+  stars = stars.map(() => new Stars());
+
+  // взаимодействие
+  setInterval(() => stars.forEach(SpaceShip => SpaceShip.move()), 20);
+
+  // использование движка
+  const engine = new Engine();
+  engine.objects.push(...stars);
--- models.js
+++ models.js
@@ -1,10 +1,24 @@
-class spiceship {
+class Stars {
 
     constructor() {
         this.el = document.createElement('div');
-        this.x = document.body.offsetWidth / 2;
-        this.y = document.body.offsetHeight - 100;
-        this.el.className = "spiceship";
+        this.speed = 10;
+        this.x = Math.round(Math.random() * (document.body.offsetWidth));
+        this.y = Math.round(Math.random() * (document.body.offsetHeight));
+        this.el.className = "stars";
         document.body.insertBefore(this.el, document.body.firstChild);
+    }
+
+    move() {
+        this.y = this.y + this.speed;
+        if (this.y > document.body.clientHeight) {
+          this.x = Math.round(Math.random() * (document.body.offsetWidth));
+          this.y = -3;
+        }
+    }
+
+    draw() {
+        this.el.style.top = this.y;
+        this.el.style.left = this.x;
     }
 }
